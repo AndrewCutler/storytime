454edca1b961c303c0dd0a45d6eba58be4cedd20
Andrew Cutler <AndrewKCutler@gmail.com>
2025-06-04 22:18:21 -0400

    initialized frontend

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Together from 'together-ai';
 
+// use environment variable, TOGETHER_API_KEY
 const together = new Together({
 	apiKey: '939d5863eb54f9fd11aaccc9f0e03831b15116f4b3f9402cdd4594aa18d768bc',
 });
@@ -8,7 +9,7 @@ const response = await together.chat.completions.create({
 	messages: [
 		{
 			role: 'user',
-			content: 'What are some fun things to do in New York?',
+			content: 'write one page of a children\'s book about a bear cub',
 		},
 	],
 	model: 'mistralai/Mistral-7B-Instruct-v0.1',
