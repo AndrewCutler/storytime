ec04135b56a7d3b1157e6f54f11dc74452a97600
Andrew Cutler <AndrewKCutler@gmail.com>
2025-06-05 23:01:30 -0400

    grab data from backend onMount

--- index.ts
+++ index.ts
@@ -1,18 +1,54 @@
 import Together from 'together-ai';
+import * as http from 'http';
 
-// use environment variable, TOGETHER_API_KEY
-const together = new Together({
-	apiKey: '939d5863eb54f9fd11aaccc9f0e03831b15116f4b3f9402cdd4594aa18d768bc',
-});
+const PORT = 8000;
+const ALLOWED_ORIGIN = 'http://localhost:5173' as const;
 
-const response = await together.chat.completions.create({
-	messages: [
-		{
-			role: 'user',
-			content: 'write one page of a children\'s book about a bear cub',
-		},
-	],
-	model: 'mistralai/Mistral-7B-Instruct-v0.1',
-});
+async function handler(req: http.IncomingMessage, res: http.ServerResponse) {
+	const {
+		headers: { origin, referer },
+	} = req;
 
-console.log(response.choices[0].message?.content);
+	if ([origin, referer].some((x) => x?.startsWith(ALLOWED_ORIGIN))) {
+		// const story = await getStory();
+		const story = 'testing';
+		res.writeHead(200, {
+			'content-type': 'text/plain',
+			'access-control-allow-origin': origin || referer,
+		});
+		res.write(story);
+	} else {
+		res.writeHead(204, { 'content-type': 'text/plain' });
+		res.write('fuck off');
+	}
+
+	res.end();
+	console.log('done');
+}
+
+const server = http.createServer(handler);
+
+server.listen(PORT);
+
+async function getStory(): Promise<string | undefined> {
+	// use environment variable, TOGETHER_API_KEY
+	const together = new Together({
+		apiKey: '939d5863eb54f9fd11aaccc9f0e03831b15116f4b3f9402cdd4594aa18d768bc',
+	});
+
+	const response = await together.chat.completions.create({
+		messages: [
+			{
+				role: 'user',
+				content: "write one page of a children's book about a bear cub",
+			},
+		],
+		model: 'mistralai/Mistral-7B-Instruct-v0.1',
+	});
+
+	console.log(response);
+
+	return response.choices[0].message?.content ?? undefined;
+}
+
+// await getStory();
